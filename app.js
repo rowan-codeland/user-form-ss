6b74d2d8af7b7a68b2fb21825c26a1d623a67ff9
Rowan <rowan.copeland@gmail.com>
2020-11-24 18:41:17 +0000

    add delay to scroll between questions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var scroll_to_three = document.getElementById('question-3');
 var scroll_to_four = document.getElementById('question-4');
 var scroll_to_five = document.getElementById('question-5');
 
+
 var q1ButtonA = document.getElementById("button-A");
 var q1ButtonB = document.getElementById("button-B");
 var q2ButtonC = document.getElementById("button-C");
@@ -15,27 +16,44 @@ var q4ButtonG = document.getElementById("button-G");
 var q4ButtonH = document.getElementById("button-H");
 var q5ButtonI = document.getElementById("button-I");
 
-q1ButtonA.addEventListener("click", function(){
-    scroll_to_two.scrollIntoView({behavior: 'smooth'});
-});
-q1ButtonB.addEventListener("click", function(){
-    scroll_to_two.scrollIntoView({behavior: 'smooth'});
-});
+q1ButtonA.addEventListener('click', function () {
+    setTimeout( () => {
+          scroll_to_two.scrollIntoView({behavior: 'smooth'});
+     }, 350)
+})
+
+q1ButtonB.addEventListener('click', function () {
+    setTimeout( () => {
+          scroll_to_two.scrollIntoView({behavior: 'smooth'});
+     }, 350)
+})
 q2ButtonC.addEventListener("click", function(){
-    scroll_to_three.scrollIntoView({behavior: 'smooth'});
+    setTimeout( () => {
+          scroll_to_three.scrollIntoView({behavior: 'smooth'});
+     }, 350)
 });
 q2ButtonD.addEventListener("click", function(){
-    scroll_to_three.scrollIntoView({behavior: 'smooth'});
+    setTimeout( () => {
+          scroll_to_three.scrollIntoView({behavior: 'smooth'});
+     }, 350)
 });
 q3ButtonE.addEventListener("click", function(){
-    scroll_to_four.scrollIntoView({behavior: 'smooth'});
+    setTimeout( () => {
+          scroll_to_four.scrollIntoView({behavior: 'smooth'});
+     }, 350)
 });
 q3ButtonF.addEventListener("click", function(){
-    scroll_to_four.scrollIntoView({behavior: 'smooth'});
+    setTimeout( () => {
+          scroll_to_four.scrollIntoView({behavior: 'smooth'});
+     }, 350)
 });
 q4ButtonG.addEventListener("click", function(){
-    scroll_to_five.scrollIntoView({behavior: 'smooth'});
+    setTimeout( () => {
+          scroll_to_five.scrollIntoView({behavior: 'smooth'});
+     }, 350)
 });
 q4ButtonH.addEventListener("click", function(){
-    scroll_to_five.scrollIntoView({behavior: 'smooth'});
+    setTimeout( () => {
+          scroll_to_five.scrollIntoView({behavior: 'smooth'});
+     }, 350)
 });
