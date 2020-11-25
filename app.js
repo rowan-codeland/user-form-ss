984db9afefe7a5b2cb421a718b83b59f8526283c
Rowan <rowan.copeland@gmail.com>
2020-11-25 00:56:31 +0000

    add footer breadcrumb and css

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-
-var bodyParent = document.getElementsByClassName("scroll-Container")
-bodyParent.childNodes
-
-var scroll_to_one = document.getElementById('question-1');
-var scroll_to_two = document.getElementById('question-2');
-var scroll_to_three = document.getElementById('question-3');
-var scroll_to_four = document.getElementById('question-4');
-var scroll_to_five = document.getElementById('question-5');
-
+var scroll_to_one = document.getElementById("question-1");
+var scroll_to_two = document.getElementById("question-2");
+var scroll_to_three = document.getElementById("question-3");
+var scroll_to_four = document.getElementById("question-4");
+var scroll_to_five = document.getElementById("question-5");
 
 var q1ButtonA = document.getElementById("button-A");
 var q1ButtonB = document.getElementById("button-B");
@@ -19,44 +14,85 @@ var q4ButtonG = document.getElementById("button-G");
 var q4ButtonH = document.getElementById("button-H");
 var q5ButtonI = document.getElementById("button-I");
 
-q1ButtonA.addEventListener('click', function () {
-    setTimeout( () => {
-          scroll_to_two.scrollIntoView({behavior: 'smooth'});
-     }, 350)
-})
+var footer1 = document.getElementById("footer1");
+var footer2 = document.getElementById("footer2");
+var footer3 = document.getElementById("footer3");
+var footer4 = document.getElementById("footer4");
+var footer5 = document.getElementById("footer5");
 
-q1ButtonB.addEventListener('click', function () {
-    setTimeout( () => {
-          scroll_to_two.scrollIntoView({behavior: 'smooth'});
-     }, 350)
-})
-q2ButtonC.addEventListener("click", function(){
-    setTimeout( () => {
-          scroll_to_three.scrollIntoView({behavior: 'smooth'});
-     }, 350)
+q1ButtonA.addEventListener("click", function () {
+  setTimeout(() => {
+    scroll_to_two.scrollIntoView({ behavior: "smooth" });
+  }, 350);
+  document.getElementById("header1").setAttribute("class", "answered");
+  document.getElementById("footer1").setAttribute("class", "selected");
 });
-q2ButtonD.addEventListener("click", function(){
-    setTimeout( () => {
-          scroll_to_three.scrollIntoView({behavior: 'smooth'});
-     }, 350)
+q1ButtonB.addEventListener("click", function () {
+  setTimeout(() => {
+    scroll_to_two.scrollIntoView({ behavior: "smooth" });
+  }, 350);
+  document.getElementById("header1").setAttribute("class", "answered");
+  document.getElementById("footer1").setAttribute("class", "selected");
 });
-q3ButtonE.addEventListener("click", function(){
-    setTimeout( () => {
-          scroll_to_four.scrollIntoView({behavior: 'smooth'});
-     }, 350)
+q2ButtonC.addEventListener("click", function () {
+  setTimeout(() => {
+    scroll_to_three.scrollIntoView({ behavior: "smooth" });
+  }, 350);
+  document.getElementById("header2").setAttribute("class", "answered");
+  document.getElementById("footer2").setAttribute("class", "selected");
 });
-q3ButtonF.addEventListener("click", function(){
-    setTimeout( () => {
-          scroll_to_four.scrollIntoView({behavior: 'smooth'});
-     }, 350)
+q2ButtonD.addEventListener("click", function () {
+  setTimeout(() => {
+    scroll_to_three.scrollIntoView({ behavior: "smooth" });
+  }, 350);
+  document.getElementById("header2").setAttribute("class", "answered");
+  document.getElementById("footer2").setAttribute("class", "selected");
 });
-q4ButtonG.addEventListener("click", function(){
-    setTimeout( () => {
-          scroll_to_five.scrollIntoView({behavior: 'smooth'});
-     }, 350)
+q3ButtonE.addEventListener("click", function () {
+  setTimeout(() => {
+    scroll_to_four.scrollIntoView({ behavior: "smooth" });
+  }, 350);
+  document.getElementById("header3").setAttribute("class", "answered");
+  document.getElementById("footer3").setAttribute("class", "selected");
 });
-q4ButtonH.addEventListener("click", function(){
-    setTimeout( () => {
-          scroll_to_five.scrollIntoView({behavior: 'smooth'});
-     }, 350)
-})
+q3ButtonF.addEventListener("click", function () {
+  setTimeout(() => {
+    scroll_to_four.scrollIntoView({ behavior: "smooth" });
+  }, 350);
+  document.getElementById("header3").setAttribute("class", "answered");
+  document.getElementById("footer3").setAttribute("class", "selected");
+});
+q4ButtonG.addEventListener("click", function () {
+  setTimeout(() => {
+    scroll_to_five.scrollIntoView({ behavior: "smooth" });
+  }, 350);
+  document.getElementById("header4").setAttribute("class", "answered");
+  document.getElementById("footer4").setAttribute("class", "selected");
+});
+q4ButtonH.addEventListener("click", function () {
+  setTimeout(() => {
+    scroll_to_five.scrollIntoView({ behavior: "smooth" });
+  }, 350);
+  document.getElementById("header4").setAttribute("class", "answered");
+  document.getElementById("footer4").setAttribute("class", "selected");
+});
+
+footer1.addEventListener("click", function () {
+  scroll_to_one.scrollIntoView({ behavior: "smooth" });
+});
+
+footer2.addEventListener("click", function () {
+  scroll_to_two.scrollIntoView({ behavior: "smooth" });
+});
+
+footer3.addEventListener("click", function () {
+  scroll_to_three.scrollIntoView({ behavior: "smooth" });
+});
+
+footer4.addEventListener("click", function () {
+  scroll_to_four.scrollIntoView({ behavior: "smooth" });
+});
+
+footer5.addEventListener("click", function () {
+  scroll_to_five.scrollIntoView({ behavior: "smooth" });
+});
